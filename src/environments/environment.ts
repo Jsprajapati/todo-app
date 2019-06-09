976c92c2519696e2d614a7c1af808ce74b9b0540
Jsprajapati <46392774+Jsprajapati@users.noreply.github.com>
2019-06-09 15:25:43 +0530

    Firebase Hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCcbrRIdyxdg-4c2NrV5RT0itlyzkcfscg",
+    authDomain: "todo-app-d09d0.firebaseapp.com",
+    databaseURL: "https://todo-app-d09d0.firebaseio.com",
+    projectId: "todo-app-d09d0",
+    storageBucket: "todo-app-d09d0.appspot.com",
+    messagingSenderId: "855617702002",
+    appId: "1:855617702002:web:883c51d0afbf43c2"
+  }
 };
 
 /*
